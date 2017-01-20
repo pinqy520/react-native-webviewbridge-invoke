d93ca770d5af2f4d8cb7accdda6e31843503bbb4
huangqi03 <huangqi03@baidu.com>
2017-01-20 15:48:25 +0800

    fix server render

--- browser.js
+++ browser.js
@@ -1,9 +1,10 @@
 "use strict";
 
 var factory_1 = require("react-native-webview-invoke/factory");
+var isBrowser = typeof window !== 'undefined';
 
 var _factory_1$default = factory_1.default(function (data) {
-    return window['WebViewBridge'].send(data);
+    isBrowser && window['WebViewBridge'].send(data);
 }),
     bind = _factory_1$default.bind,
     define = _factory_1$default.define,
@@ -14,15 +15,17 @@ var _factory_1$default = factory_1.default(function (data) {
     removeEventListener = _factory_1$default.removeEventListener,
     isConnect = _factory_1$default.isConnect;
 
-if (window['WebViewBridge']) {
-    ready();
-} else {
-    window.addEventListener('webviewbridge:init', function () {
-        if (window['WebViewBridge']) {
-            window['WebViewBridge'].addMessageListener(listener);
-            ready();
-        }
-    });
+if (isBrowser) {
+    if (window['WebViewBridge']) {
+        ready();
+    } else {
+        window.addEventListener('webviewbridge:init', function () {
+            if (window['WebViewBridge']) {
+                window['WebViewBridge'].addMessageListener(listener);
+                ready();
+            }
+        });
+    }
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
--- browser.ts
+++ browser.ts
@@ -1,19 +1,22 @@
 import createMessager, { IPayload } from 'react-native-webview-invoke/factory'
 
+const isBrowser = typeof window !== 'undefined'
 
 const { bind, define, listener, ready, fn, addEventListener, removeEventListener, isConnect } = createMessager(
-    (data: any) => (window as any)['WebViewBridge'].send(data)
+    (data: any) => { isBrowser && (window as any)['WebViewBridge'].send(data) }
 )
 
-if ((window as any)['WebViewBridge']) {
-    ready()
-} else {
-    window.addEventListener('webviewbridge:init', function () {
-        if ((window as any)['WebViewBridge']) {
-            (window as any)['WebViewBridge'].addMessageListener(listener)
-            ready()
-        }
-    })
+if (isBrowser) {
+    if ((window as any)['WebViewBridge']) {
+        ready()
+    } else {
+        window.addEventListener('webviewbridge:init', function () {
+            if ((window as any)['WebViewBridge']) {
+                (window as any)['WebViewBridge'].addMessageListener(listener)
+                ready()
+            }
+        })
+    }
 }
 
 export default {
